860a6e8fae8ae1a804f24b05fe7712fa74ea2114
Vianney <vianney_mb@yahoo.fr>
2025-03-04 17:51:12 +0100

    first commit

---
+++ src/main/java/com/springbootTP/springbootPostgreSQL/SpringbootPostgreSqlApplication.java
@@ -0,0 +1,13 @@
+package com.springbootTP.springbootPostgreSQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPostgreSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootPostgreSqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springbootTP/springbootPostgreSQL/SpringbootPostgreSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootTP.springbootPostgreSQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPostgreSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
